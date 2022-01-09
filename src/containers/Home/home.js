5d19511b8d394e546eee985fd47b1f55eee3526a
Vinay Vinnakota <vinay.vinnakota@tadigital.com>
2022-01-09 14:39:35 +0530

    Initial setup

---
+++ src/containers/Home/home.js
@@ -0,0 +1,23 @@
+import React  from 'react';
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div>
+                ...topribbon
+                ...logo
+                <header>
+                    <h1>test</h1>
+                </header>
+                <main>
+                    <p>test</p>
+                </main>
+                <footer>
+
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Home;
